04cdd39904957c4a11084e5d85d34c994d3f2a57
Camilo Abbate <camello187@gmail.com>
2022-04-28 15:25:11 -0700

    more

--- lectures/week5/week_5_script_spatial.R
+++ lectures/week5/week_5_script_spatial.R
@@ -81,6 +81,10 @@ coc_totals <- coc_totals[-25,]
 
 coc_NY_state <- tibble(coc_names,coc_totals)
 
+#remove:
+
+rm(coc_names,coc_totals)
+
 coc_NY_state <- coc_NY_state %>%
     tidyr::extract(value, into = "total_FED_money", ".{1,}\\$(.{1,}).{1,}", remove = F)
 
@@ -116,6 +120,10 @@ plot_usmap("counties", fill = "yellow", alpha = 0.25,
 plot_usmap("counties", fill = "yellow", alpha = 0.25,
            include = c(.south_region), exclude = c(.east_south_central))
 
+# EXERCISE 
+plot_usmap("counties", fill = "yellow", alpha = 0.25,
+           include = c(.south_region), exclude = "FL")
+
 
 #Only NY state, in blue:
 
@@ -129,24 +137,127 @@ plot_usmap("counties", fill = "blue", alpha = 0.25,
 #Suppose we manage to use the data from the pdf and distribute "roughly" into the counties:
 ny_hm <- read_csv('lectures/week5/ny_hmfm.csv')
 
-usmap::plot_usmap(data=ny_hm,include="NY", values = "overall_homeless", color = "grey") #+
+usmap::plot_usmap(data=ny_hm,include="NY", values = "overall_homeless") #+
   # ggplot2::scale_fill_gradient(low="green",high="darkgreen") + labs(title="Hi")
+
+
+
+#### HOMEWORK!
+#exclude nyc
+usmap::plot_usmap(data=ny_hm[-c(3,24,31,41,43),],include="NY", values = "overall_homeless")
+
+#exclude nyc and Nassau(30), Suffolk(52) and Westchester(60)
+usmap::plot_usmap(data=ny_hm[-c(3,24,30,31,41,43,52,60),],include="NY", values = "overall_homeless")
+
+
+#### HOMEWORK ENDS!
 
 #Not a lot of info.. let's standarize?
 ny_hm <- ny_hm %>% mutate(homeless_std = (overall_homeless - mean(overall_homeless))/sd(overall_homeless))
 
 
-usmap::plot_usmap(data=ny_hm,include="NY", values = "homeless_std", color = "grey") #+
+
+#how much money in each county:
+usmap::plot_usmap(data=ny_hm,include="NY", values = "fed_money", color = "grey")
+
 
 #let's try the ratio of money per homeless:
 
-ny_hm <- ny_hm %>% mutate(dollar_p_hm = fed_money/overall_homeless) 
+ny_hm <- ny_hm %>% mutate(dollar_p_hm = fed_money/overall_homeless)
 
 usmap::plot_usmap(data=ny_hm,include="NY", values = "dollar_p_hm", color = "grey") +
- labs(title="Federal dollar per homeless individual - NY State 2020")
+ labs(title="Federal dollar per homeless - NY State 2020")
+
+############# break
+#Now SF package!? YEAH
+
+#SF: 
+#Provides a set of tools for working with geospatial vectors, i.e. points, lines, polygons, etc
+
+#North Carolina data
+demo(nc, ask = FALSE, echo = FALSE)
+nc <- nc %>% janitor::clean_names()
+class(nc)
+plot(nc)
+plot(nc["area"])
+plot(nc["perimeter"])
+
+plot(nc$geom)
+plot(st_geometry(nc))
 
 
-#Now SF package!?
+#Let's do a little bit ourselves with NY data:
+load("lectures/week5/ny_spatial.RData")
+
+class(ny_for_maps)
+
+
+View(ny_for_maps)
+View(ny_for_maps[,1:10])   # TAKES 3 HOURS TO OPEN, geometry doesn't disappear
+
+ny_no_geometry <- select(as.data.frame(ny_for_maps), -geometry)
+
+View(ny_no_geometry)
+
+plot(ny_for_maps['coc_area'])
+
+#What we are going to do next is try to simulate a similar graph as before when we did:
+usmap::plot_usmap(data=ny_hm,include="NY", values = "fed_money", color = "grey")
+
+#so, check this out:
+#or better, explain left join and stuff, and everything we will do:
+#start with:
+
+coc_NY_state$coc_name
+
+ny_for_maps$cocnum
+
+#we can match those huh? Like, for each cocnum in ny_for_maps, we can match it with coc_NY_state$coc_name
+#and back up the federal money from it!
+#we need the name of the columns to be the same! (doesn't matter that we have different number of rows!)
+#we are going to use a command called merge
+
+#Two things to do:
+#extract the first 6 characters of coc_NY_state$coc_name and later set everything to lowercase
+
+#extract: str_sub(coc_NY_state$coc_name,1,6)
+#worst: coc_NY_state$coc_number <- str_sub(coc_NY_state$coc_name,1,6)
+#better:
+
+coc_NY_state <- coc_NY_state %>% mutate(cocnum = str_sub(coc_name,1,6))
+
+ny_for_maps$cocnum
+
+ny_for_maps$cocnum <- tolower(ny_for_maps$cocnum)
+
+colnames(coc_NY_state)
+
+colnames(ny_for_maps)
+
+#jumping waaay ahead of ourselves
+new_york_state <- left_join(ny_for_maps,coc_NY_state,by="cocnum")
+
+plot(new_york_state["total_FED_money"]) #missing data
+
+#The City of New York is made up of five boroughs. Each borough is a county of New York State.
+
+plot(st_geometry(new_york_state), col = sf.colors(12, categorical = TRUE), border = 'grey', 
+     axes = TRUE)
+
+options(sf_max.plot=1)
+plot(new_york_state)
+plot(new_york_state["area"])
+
+
+#next one is SUPER optional
+
+ggplot() + 
+  geom_sf(data = new_york_state, aes(fill = "area"))
+
+
+# OTHER STUFF, SUPER SUPPEEER OPTIONAL AND MAYBE!!!
+
+mapview(breweries)
 
 nyc_arrests = read_csv('lectures/week5/nyc_arrests.csv')
 
@@ -170,7 +281,7 @@ ggplot(total_crime_by_age,aes(x=date, y = tot_crime, group = age_group, colour =
 
 library(ggmap)
 bbox<- c(left = -74.35, bottom = 40.498, right = -73.687, top = 40.90)
-nyc <-get_stamenmap(bbox, zoom = 11, maptype = "terrain")
+nyc <- get_stamenmap(bbox, zoom = 11, maptype = "terrain")
 
 ggmap(nyc) +
   geom_point(data = nyc_arrests %>% filter(ofns_desc=='ROBBERY' & year==2010),aes(longitude,latitude),
@@ -186,41 +297,4 @@ ggmap(nyc) +
   theme(legend.position = c(0.01, 0.5)) +
   labs(color = "Perpetrator race",
        title = "Robberies by race in NYC")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-mapview(breweries)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
